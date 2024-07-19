563773882d4c84b2322301b499d3dc209a74db0a
Daniel-Alvarenga <daniel07alvarenga2023@gmail.com>
2024-07-19 12:32:58 -0300

    Fix check error

--- src/modules/background.js
+++ src/modules/background.js
@@ -1,14 +1,41 @@
-chrome.storage.sync.get(['githubUsername', 'githubToken'], function(data) {
-    const username = data.githubUsername;
-    const token = data.githubToken;
-  
-    if (!username || !token) {
-        console.log('GitHub credentials not found.');
-        return;
-    }
-});
+const followers = document.querySelectorAll('.d-table.table-fixed');
 
-async function checkFollowerStatus(userToCheck) {
+async function processFollowers(username, token) {
+    for (const follower of followers) {
+        const userLink = follower.querySelector('a[data-hovercard-type="user"]');
+
+        if (userLink) {
+            const userToCheck = userLink.getAttribute('href').substring(1);
+
+            const isBackFollower = await isFollower(userToCheck, username, token);
+
+            const status = isBackFollower ? 'Follows you' : 'Doesn\'t follow you';
+
+            const statusElement = document.createElement('span');
+            statusElement.textContent = status;
+            statusElement.style.fontSize = 'small';
+            statusElement.style.color = 'gray';
+            statusElement.alt = 'Follower Tracker';
+
+            const receivBlock = follower.querySelector('div.d-table-cell.col-9.v-align-top.pr-3');
+            if (receivBlock) {
+                receivBlock.appendChild(statusElement);
+            }
+        }
+    }
+}
+
+async function isFollower(userToCheck, username, token){
+    try {
+        const isFollower = await consultFollowStat(userToCheck, username, token);
+        return isFollower;
+    } catch (error) {
+        console.error('Error:', error);
+        return false;
+    }
+}
+
+async function consultFollowStat(userToCheck, username, token) {
     let page = 1;
     let followers = [];
 
@@ -29,15 +56,25 @@ async function checkFollowerStatus(userToCheck) {
     return followers.some(follower => follower.login === userToCheck);
 }
 
-browser.runtime.onMessage.addListener(async function(request, sender, sendResponse) {
-    if (request.action === "checkFollowerStatus") {
-        try {
-            const isFollower = await checkFollowerStatus(request.username);
-            sendResponse({isFollower});
-        } catch (error) {
-            console.error('Error:', error);
-            sendResponse({isFollower: false});
-        }
-        return true;
+chrome.storage.local.get(['githubUsername', 'githubToken'], function(data) {
+    console.log('Storage data retrieved:', data);
+    var isExtensionOn = false;
+
+    chrome.storage.local.get('isExtensionOn', function(data) {
+        isExtensionOn = data.isExtensionOn;
+    });
+
+    const username = data.githubUsername;
+    const token = data.githubToken;
+
+    if (!username || !token) {
+        console.log('GitHub credentials not found.');
+        return;
+    } else {
+        console.log('Extensão ativa:', isExtensionOn);
+        console.log('GitHub Username:', username);
+        console.log('GitHub Token:', token);
+
+        processFollowers(username, token);
     }
 });
