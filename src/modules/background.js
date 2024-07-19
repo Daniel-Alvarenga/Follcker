45f39d8fe796a4ad2bf9961229395cb125ba4827
Daniel-Alvarenga <daniel07alvarenga2023@gmail.com>
2024-07-19 18:30:01 -0300

    Code style

--- src/modules/background.js
+++ src/modules/background.js
@@ -1,98 +1,103 @@
 async function processFollowers(username, token) {
-    const followers = document.querySelectorAll('.d-table.table-fixed');
-    console.log("Processando...")
-    for (const follower of followers) {
-        const userLink = follower.querySelector('a[data-hovercard-type="user"]');
+  const followers = document.querySelectorAll(".d-table.table-fixed");
+  console.log("Processando...");
+  for (const follower of followers) {
+    const userLink = follower.querySelector('a[data-hovercard-type="user"]');
 
-        if (userLink) {
-            const userToCheck = userLink.getAttribute('href').substring(1);
+    if (userLink) {
+      const userToCheck = userLink.getAttribute("href").substring(1);
 
-            const isBackFollower = await isFollower(userToCheck, username, token);
+      const isBackFollower = await isFollower(userToCheck, username, token);
 
-            const status = isBackFollower ? 'Follows you' : 'Doesn\'t follow you';
+      const status = isBackFollower ? "(follows you)" : "";
 
-            const statusElement = document.createElement('span');
-            statusElement.textContent = status;
-            statusElement.style.fontSize = 'small';
-            statusElement.style.color = 'gray';
-            statusElement.alt = 'Follcker';
-            statusElement.class = 'follcker-element';
+      const statusElement = document.createElement("span");
+      statusElement.textContent = status;
+      statusElement.style.fontSize = "small";
+      statusElement.style.color = "gray";
+      statusElement.alt = "Follcker";
+      statusElement.class = "follcker-element";
 
-            const receivBlock = follower.querySelector('div.d-table-cell.col-9.v-align-top.pr-3');
-            if (receivBlock) {
-                receivBlock.appendChild(statusElement);
-            }
-        }
+      const receivBlock = follower.querySelector(
+        "div.d-table-cell.col-2.v-align-top.text-right"
+      );
+      if (receivBlock) {
+        receivBlock.appendChild(statusElement);
+      }
     }
+  }
 }
 
-async function isFollower(userToCheck, username, token){
-    try {
-        const isFollower = await consultFollowStat(userToCheck, username, token);
-        return isFollower;
-    } catch (error) {
-        console.error('Error:', error);
-        return false;
-    }
+async function isFollower(userToCheck, username, token) {
+  try {
+    const isFollower = await consultFollowStat(userToCheck, username, token);
+    return isFollower;
+  } catch (error) {
+    console.error("Error:", error);
+    return false;
+  }
 }
 
 async function consultFollowStat(userToCheck, username, token) {
-    let page = 1;
-    let followers = [];
+  let page = 1;
+  let followers = [];
 
-    while (true) {
-        const response = await fetch(`https://api.github.com/users/${username}/followers?page=${page}`, {
-            headers: {
-                'Authorization': `token ${token}`
-            }
-        });
-        const data = await response.json();
+  while (true) {
+    const response = await fetch(
+      `https://api.github.com/users/${username}/followers?page=${page}`,
+      {
+        headers: {
+          Authorization: `token ${token}`,
+        },
+      }
+    );
+    const data = await response.json();
 
-        if (data.length === 0) break;
+    if (data.length === 0) break;
 
-        followers = followers.concat(data);
-        page++;
-    }
+    followers = followers.concat(data);
+    page++;
+  }
 
-    return followers.some(follower => follower.login === userToCheck);
+  return followers.some((follower) => follower.login === userToCheck);
 }
 
-function trackFollowers(){
-    browser.storage.local.get(['githubUsername', 'githubToken'], function(data) {
-        console.log('Storage data retrieved:', data);
-    
-        const username = data.githubUsername;
-        const token = data.githubToken;
-    
-        if (!username || !token) {
-            return;
-        } else {
-            browser.storage.local.get('isExtensionOn', function(data) {
-                removeElements();
+function trackFollowers() {
+  browser.storage.local.get(["githubUsername", "githubToken"], function (data) {
+    console.log("Storage data retrieved:", data);
 
-                if(data.isExtensionOn == true){
-                    processFollowers(username, token);
-                }
-            });        
+    const username = data.githubUsername;
+    const token = data.githubToken;
+
+    if (!username || !token) {
+      return;
+    } else {
+      browser.storage.local.get("isExtensionOn", function (data) {
+        removeElements();
+
+        if (data.isExtensionOn == true) {
+          processFollowers(username, token);
         }
-    });
+      });
+    }
+  });
 }
 
-browser.runtime.onMessage.addListener(function(request) {
-    removeElements();
-    
-    if (request.action === "on"){
-        trackFollowers();
-    }
+browser.runtime.onMessage.addListener(function (request) {
+  removeElements();
+
+  if (request.action === "on") {
+    trackFollowers();
+  }
 });
 
 function removeElements() {
-    console.log("Limpando elementos já colocados")
-    const elements = document.querySelectorAll('.follcker-element');
-    
-    elements.forEach(element => {
-        element.remove();
-    });
+  console.log("Limpando elementos já colocados");
+  const elements = document.querySelectorAll(".follcker-element");
+
+  elements.forEach((element) => {
+    element.remove();
+  });
 }
 
 trackFollowers();
--- src/modules/script.js
+++ src/modules/script.js
@@ -1,56 +1,62 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const toggleButton = document.getElementById('toggle-button');
-  const statusText = document.getElementById('status-text');
-  const credentialsForm = document.getElementById('credentials-form');
-  const usernameInput = document.getElementById('username');
-  const tokenInput = document.getElementById('token');
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("toggle-button");
+  const statusText = document.getElementById("status-text");
+  const credentialsForm = document.getElementById("credentials-form");
+  const usernameInput = document.getElementById("username");
+  const tokenInput = document.getElementById("token");
 
-  browser.storage.local.get(['isExtensionOn', 'githubUsername', 'githubToken'], function(data) {
-    if (data.isExtensionOn) {
-      toggleButton.classList.add('on');
-      statusText.textContent = 'ON';
-    } else {
-      toggleButton.classList.remove('on');
-      statusText.textContent = 'OFF';
+  browser.storage.local.get(
+    ["isExtensionOn", "githubUsername", "githubToken"],
+    function (data) {
+      if (data.isExtensionOn) {
+        toggleButton.classList.add("on");
+        statusText.textContent = "ON";
+      } else {
+        toggleButton.classList.remove("on");
+        statusText.textContent = "OFF";
+      }
+
+      if (data.githubUsername) {
+        usernameInput.value = data.githubUsername;
+      }
+
+      if (data.githubToken) {
+        tokenInput.value = data.githubToken;
+      }
     }
+  );
 
-    if (data.githubUsername) {
-      usernameInput.value = data.githubUsername;
-    }
-
-    if (data.githubToken) {
-      tokenInput.value = data.githubToken;
-    }
-  });
-
-  toggleButton.addEventListener('click', function() {
-    browser.storage.local.get('isExtensionOn', function(data) {
+  toggleButton.addEventListener("click", function () {
+    browser.storage.local.get("isExtensionOn", function (data) {
       const isExtensionOn = !data.isExtensionOn;
-      browser.storage.local.set({ isExtensionOn: isExtensionOn }, function() {
+      browser.storage.local.set({ isExtensionOn: isExtensionOn }, function () {
         if (isExtensionOn) {
-          toggleButton.classList.add('on');
-          statusText.textContent = 'ON';
+          toggleButton.classList.add("on");
+          statusText.textContent = "ON";
 
-          browser.runtime.sendMessage({action: "on"});
+          browser.runtime.sendMessage({ action: "on" });
         } else {
-          toggleButton.classList.remove('on');
-          statusText.textContent = 'OFF';
+          toggleButton.classList.remove("on");
+          statusText.textContent = "OFF";
 
-          browser.runtime.sendMessage({action: "off"});
+          browser.runtime.sendMessage({ action: "off" });
         }
       });
     });
   });
 
-  credentialsForm.addEventListener('submit', function(event) {
+  credentialsForm.addEventListener("submit", function (event) {
     event.preventDefault();
     const githubUsername = usernameInput.value;
     const githubToken = tokenInput.value;
-    browser.storage.local.set({
-      githubUsername: githubUsername,
-      githubToken: githubToken
-    }, function() {
-      alert('Credentials saved successfully!');
-    });
+    browser.storage.local.set(
+      {
+        githubUsername: githubUsername,
+        githubToken: githubToken,
+      },
+      function () {
+        alert("Credentials saved successfully!");
+      }
+    );
   });
 });
